2b3b8c2d4dac6c7f3961832cbd9bcaa90ca9f845
denervalspelizario <denervalspelizario@gmail.com>
2023-02-28 14:50:09 -0300

    estudo alura-oracle revisao variaveis js

---
+++ 1_JavaScript_ExplorandoLingaguem/2_Variaveis.js
@@ -0,0 +1,27 @@
+console.log("Trabalhando com variáveis\n")
+
+const idade = 26;
+console.log(idade)
+
+console.log("\n")
+console.log(idade + 2)
+console.log(idade - 2)
+console.log(idade / 2)
+console.log(idade * 2)
+
+
+const idadeSomada = idade + 2
+
+console.log("\n")
+console.log(idadeSomada)
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1_JavaScript_ExplorandoLingaguem/3_TrabalhandoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com Variáveis");
+
+
+let idade = 29
+let Idade = 30
+const nome = "Denerval"
+let ano = 2020
+
+console.log(idade);
+console.log(Idade);
+console.log(nome);
+console.log(ano)

c4dd9c4625e6f13e6fd4032d7b5a37392681c1cf
denervalspelizario <denervalspelizario@gmail.com>
2023-03-08 11:29:47 -0300

    estudo alura-frontend revisao acesso a elemento pelo dom e manipulacao do mesmo por input

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,21 +1,32 @@
 
+const criarTarefa = (evento) => { // funcao disparada apó o clique (ler linha 20)
+
+   evento.preventDefault() // funcao que garante o comportamento padrao essa funcao garante o msm comportamento em diferentes
+                           // browser evitando comprtamento diferentes entre browsers 
+
+   const  input =  document.querySelector('[data-form-input]') // variavel que recebe dom de item input (linha 25 de index.html)
+
+   const valor = input.value // variavel valor recebe dado(value) de input digitado ou seja foi digitado algo este valor sera 
+                                  // recebido por esta variável     
+
+   console.log(valor) // imprimi no console o valor(value) digitado no input(linha 13) 
+
+}
+
+
+
+
+
 const  novaTarefa =  document.querySelector('[data-form-button]') // variavel que recebe dom de item button (linha 26 de index.html)
 
 //console.log(input)  confirmando no atravez do console.log se esta fucnionando o dom
 //console.log(novaTarefa)  confirmando no atravez do console.log se esta fucnionando o dom
 
 
-novaTarefa.addEventListener('click', () => {     // funcao que pega o dados de novaTarefa(recebeu o button linha 02)
-                                                 // ao clicar(evento) dispara uma  funcao anonima                      
+novaTarefa.addEventListener('click', criarTarefa)  // na funcao recebe o click (acao do cique la no botao linha 26 index.html) 
+                                                   // e o segundo parametro é a funcao que vai dar ação depois do clique (leia linha 2)   
+                                                   // addEventListener para escutar eventos no elemento
 
-        const  input =  document.querySelector('[data-form-input]') // variavel que recebe dom de item input (linha 25 de index.html)
-
-        const valor = input.value // variavel valor recebe dado(value) de input digitado ou seja foi digitado algo este valor sera 
-                                  // recebido por esta variável     
-
-        console.log(valor) // imprimi no console o valor(value) digitado no input(linha 13)                           
-    }    
- )
 
  /*
     Explicando  pega-se o elemento formulario atravez do dom e adiciona em uma variável novaTarefa
